6879659ef746939373bd884c0c85dfe8ea4db388
Mohamed Amine ELGNAOUI <mohamed.amine.elgnaoui@gmail.com>
2020-07-15 00:26:52 +0200

    TP 1.2 Configuration JAVA

--- src/main/java/dev/dao/PlatDaoFichier.java
+++ src/main/java/dev/dao/PlatDaoFichier.java
@@ -10,13 +10,18 @@ import java.nio.file.StandardOpenOption;
 import java.util.Collections;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+
 import static java.util.stream.Collectors.toList;
 
+
+@Repository 
 public class PlatDaoFichier implements IPlatDao {
 
     private String fichierStockage;
 
-    public PlatDaoFichier(String fichierStockage) {
+    public PlatDaoFichier( 	@Value("${pathFile}") String fichierStockage)  {
         this.fichierStockage = fichierStockage;
         if (!Files.exists(Paths.get(this.fichierStockage))) {
             try {
