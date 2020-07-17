cf06e2b860740741becb021fef0cac1194940f27
Mohamed Amine ELGNAOUI <mohamed.amine.elgnaoui@gmail.com>
2020-07-17 09:48:40 +0200

    TP4.1 Spring JDBC : Fin

--- src/main/java/dev/dao/PlatDaoJdbc.java
+++ src/main/java/dev/dao/PlatDaoJdbc.java
@@ -2,20 +2,33 @@ package dev.dao;
 
 import java.util.List;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
 import dev.entite.Plat;
 
-public class PlatDaoJdbc implements IPlatDao {
+@Repository
+@Profile("jdbc")
+public class PlatDaoJdbc implements IPlatDao{
+
+	private JdbcTemplate jdbcTemplate;
+
+	public PlatDaoJdbc(DataSource ds) {
+        jdbcTemplate = new JdbcTemplate(ds);
+    }
 
 	@Override
 	public List<Plat> listerPlats() {
 		// TODO Auto-generated method stub
-		return null;
+		return jdbcTemplate.query("SELECT * FROM PLAT", new PlatRowMapper());
 	}
 
 	@Override
 	public void ajouterPlat(String nomPlat, Integer prixPlat) {
-		// TODO Auto-generated method stub
-		
+		jdbcTemplate.update("INSERT INTO PLAT (NOM, PRIX) VALUES (?, ?)", nomPlat,prixPlat);
+		return;
 	}
-
 }
